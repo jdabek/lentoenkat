78aa6d7a9f7693b8167a964ae3e6f6ff8329d444
jusju <jukka.juslin@haaga-helia.fi>
2022-12-27 13:05:41 +0200

    prosessoi lomaketietoa

--- jukkaprosessoi_kirjautuminen.php
+++ jukkaprosessoi_kirjautuminen.php
@@ -2,8 +2,38 @@
 
    if($_GET['kayttajatunnus'] == 'jukka') {
     print("MENI SISAAN");
+    $kahva = sqlite_yhdista();
+    $kahva->exec('CREATE TABLE foo (bar STRING)');
+
    }
 
-   
+
+   function sqlite_yhdista()
+   {
+       $kahva = new SQLite3("./lentoenkat.sqlite");
+       return $kahva;
+   }
+
+   function sqlite_query($dbhandle,$query)
+   {
+       $array['dbhandle'] = $dbhandle;
+       $array['query'] = $query;
+       $result = $dbhandle->query($query);
+       return $result;
+   }
+   function sqlite_fetch_array(&$result,$type)
+   {
+       #Get Columns
+       $i = 0;
+       while ($result->columnName($i))
+       {
+           $columns[ ] = $result->columnName($i);
+           $i++;
+       }
+      
+       $resx = $result->fetchArray(SQLITE3_ASSOC);
+       return $resx;
+   }
+
 
 ?>
